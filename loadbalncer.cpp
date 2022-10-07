9764b15246586d18187b2d810404f515276199aa
shanebrown013 <shanebrown013@gmail.com>
2022-10-07 12:04:41 -0500

    Started

--- loadbalncer.cpp
+++ loadbalncer.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
 
+    // Get the number of servers from user
+    int numOfServers = 0;
+    cout << "Enter the number of servers: ";
+    cin >> numOfServers;
+
+    int timeRun = 0;
+    cout << "Enter the time you want the load balancer (seconds): ";
+    cin >> timeRun;
     
 }
